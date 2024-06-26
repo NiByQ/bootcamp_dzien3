4049a37deb9a5879b6307495a1d88a9b1a8076ba
NiByQ <nikobycz@gmail.com>
2024-06-26 19:21:48 +0200

    mam problemy

--- src/dzien3_frontend/src/components/Blog.vue
+++ src/dzien3_frontend/src/components/Blog.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        siema blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-blue-600">Wpisy na bloga</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="float-right bg-blue-600 rounded-sm text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="(wpis, index) in wpisy" class="grid drop-shadow-xl bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+             </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+            <button @click="dodajWpisy" class="float-right bg-blue-600 rounded-sm text-white p-4">dodaj</button>
+        </div>
+        </div>
     </div>
 </template>
 
