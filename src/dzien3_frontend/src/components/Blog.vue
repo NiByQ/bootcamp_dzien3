a21df43b23c66e3c258d5afd48f52be4267417fe
NiByQ <nikobycz@gmail.com>
2024-06-26 17:37:40 +0200

    dodałem edytowanie tekstu

---
+++ src/dzien3_frontend/src/components/Blog.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click="pobierzWpisy">refresh</button>
+        siema blog!
+        {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
+    </div>
+</template>
+
+<script>
+import { dzien3_backend } from 'declarations/dzien3_backend/index';
+
+export default {
+    data() {
+        return {
+            wpisy: [],
+            nowyBlog: ""
+        }
+    },
+    methods: {
+        async dodajWpisy() {
+            await dzien3_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy() {
+            this.wpisy = await dzien3_backend.odczytaj_wpisy();
+        }
+    },
+
+}
+</script>
